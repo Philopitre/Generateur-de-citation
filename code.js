cc77db977b307bc12730342734e8da4849dd3c79
Christophe Bral <92055523+Philopitre@users.noreply.github.com>
2024-02-18 08:22:13 +0100

    Add citation

--- code.js
+++ code.js
@@ -25,7 +25,8 @@ let citations = [
   ["Pour gagner votre vie, apprenez à l’école. Pour gagner une fortune, apprenez par vous-même.", "Brian Tracy"],
   ["Les gagnants trouvent des moyens, les perdants des excuses…", "F. D. Roosevelt"],
   ["Vous n’êtes jamais trop vieux pour vous fixer de nouveaux buts, ou rendre vos rêves réalité.", "C.S. Lewis"],
-  ["Un pessimiste voit la difficulté dans chaque opportunité. Un optimiste voit une opportunité dans chaque difficulté.", "Winston Churchill"]
+  ["Un pessimiste voit la difficulté dans chaque opportunité. Un optimiste voit une opportunité dans chaque difficulté.", "Winston Churchill"],
+  ["L'avantage d'être intelligent, c'est qu'on peut toujours faire l'imbécile, alors que l'inverse est totalement impossible.", "Woody Allen"]
 ];
 
 // Fonction permettant de générer un nombre aléatoire
